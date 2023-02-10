dddefcb8652ca7aea356e6fa9db7e3aac94febf2
Snehal <snehal9140@gmail.com>
2023-02-10 10:57:43 +0530

    added nodejs and flask app

--- pythonflask/app.py
+++ pythonflask/app.py
@@ -1,6 +1,11 @@
+
 from flask import Flask
 app = Flask(__name__)
-
+  
 @app.route('/')
-def hello_world():
-    return 'Hello, Docker!'
+def hello():
+    return "welcome to the flask tutorials"
+  
+  
+if __name__ == "__main__":
+    app.run(host ='0.0.0.0', port = 5001, debug = True) 
